902041254c590d20297efd3402f8750511194041
awosborn <alexander.w.osborne@gmail.com>
2021-02-11 17:04:20 -0700

    Add item Model test

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,8 @@
 class Item < ApplicationRecord
+  validates_presence_of :name, presence: true
+  validates_presence_of :description, presence: true
+  validates_presence_of :unit_price, presence: true
+  validates_presence_of :merchant_id, presence: true
+  belongs_to :merchant
+  has_many :invoice_items
 end
