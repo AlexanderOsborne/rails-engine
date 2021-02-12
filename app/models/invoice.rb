e58db8a274178aa7e4835b07af537ea9ce5f2da2
awosborn <alexander.w.osborne@gmail.com>
2021-02-11 17:16:39 -0700

    Add invoice model test

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,2 +1,7 @@
 class Invoice < ApplicationRecord
+  validates_presence_of :customer_id, presence: true
+  validates_presence_of :merchant_id, presence: true
+  belongs_to :customer
+  belongs_to :merchant
+  enum status: [:pending, :packaged, :shipped, :returned]
 end
