35454b34df50f717c3225d6ed7be81deba4f629f
awosborn <alexander.w.osborne@gmail.com>
2021-02-11 17:19:46 -0700

    Add transaction model test

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,7 @@
 class Transaction < ApplicationRecord
+  validates_presence_of :invoice_id, presence: true
+  validates_presence_of :credit_card_number, presence: true
+  validates_presence_of :credit_card_expiration_date, presence: true
+  belongs_to :invoice
+  enum status: [:success, :failed, :refunded]
 end
