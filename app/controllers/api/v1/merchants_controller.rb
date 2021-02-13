46ab082d8201db29c73b4843f3a0cbffe6bab801
awosborn <alexander.w.osborne@gmail.com>
2021-02-13 13:42:14 -0700

    Find by Merchant endpoint

---
+++ app/controllers/api/v1/merchants_controller.rb
@@ -0,0 +1,10 @@
+class Api::V1::MerchantsController < ApplicationController
+
+  def index
+  end
+
+  def show
+    render json: MerchantSerializer.new(Merchant.find(params[:id]))
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,22 +4,24 @@ Rails.application.routes.draw do
     namespace :merchants do
       resources :find, controller: 'search', only: [:index]
       resources :find_all, controller: 'search', only: [:index]
-
-    end
-    resources :merchants, module: :items, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
-      resources :merchants, only: [:index]
     end
 
-    namespace :items do
-      resources :find_all, controller: 'search', only: [:index]
-    end
+    resources :merchants, only:[:show]
+    # resources :merchants, module: :items, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
+    #   resources :merchants, only: [:index]
+    # end
+    
 
-    resources :revenue, module: :revenue, only: [:index] do
-      resources :merchants, only: [:index, :show]
-      resources :items, only: [:index]
-      resources :unshipped, only: [:index]
-      resources :weekly, only: [:index]
-      end
+    # namespace :items do
+    #   resources :find_all, controller: 'search', only: [:index]
+    # end
+
+    # resources :revenue, module: :revenue, only: [:index] do
+    #   resources :merchants, only: [:index, :show]
+    #   resources :items, only: [:index]
+    #   resources :unshipped, only: [:index]
+    #   resources :weekly, only: [:index]
+    #   end
     end    
   end
 end
