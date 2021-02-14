717cc964681f4d153fb63e734c109e75c1deab7e
awosborn <alexander.w.osborne@gmail.com>
2021-02-13 17:03:04 -0700

    Add functional index function to merchants controller

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -2,10 +2,10 @@ class Api::V1::MerchantsController < ApplicationController
 
   def index
     begin
-      render json: MerchantSerializer.new(Merchant.all)
+      render json: MerchantSerializer.new(Merchant.select_records(params))
       rescue
       render json: {"error" => {}}, status:404
-      end
+    end
   end
 
   def show
