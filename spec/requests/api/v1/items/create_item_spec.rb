be7b1e783d3e778fb2d78acb84f065939c439d40
awosborn <alexander.w.osborne@gmail.com>
2021-02-14 10:58:11 -0700

    Update create_item testing

--- spec/requests/api/v1/items/create_item_spec.rb
+++ spec/requests/api/v1/items/create_item_spec.rb
@@ -31,4 +31,21 @@ RSpec.describe 'create an item' do
     expect(item[:data].keys).to match_array(%i[id type attributes])
     expect(item[:data][:attributes].keys).to match_array(%i[name description unit_price merchant_id])
     end
+
+    it 'ignores any additional attributes sent by the user' do
+      merchant_id = create(:merchant).id
+      item_params = attributes_for(:item).merge(merchant_id: merchant_id, model_number: '28P1817074')
+  
+      headers = {'CONTENT_TYPE' => 'application/json'}
+  
+      post '/api/v1/items', headers: headers, params: JSON.generate(item: item_params)
+  
+      expect(response).to be_successful
+  
+      created_item = Item.last
+      expect(created_item).not_to have_attribute(:model_number)
+  
+      item = JSON.parse(response.body, symbolize_names: true)
+      expect(item[:data][:attributes].keys).to match_array(%i[name description unit_price merchant_id])
+    end
   end
