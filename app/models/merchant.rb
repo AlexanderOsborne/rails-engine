82768cce3672d6bde280c50979e523bbaa6d4919
awosborn <alexander.w.osborne@gmail.com>
2021-02-16 15:29:32 -0700

    Add find_one merchant model method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,5 +1,9 @@
 class Merchant < ApplicationRecord
   validates_presence_of :name, presence: true
-  has_many :items
+  has_many :items, dependent: :destroy
   has_many :invoices
+
+  def self.find_one(query)
+    where('LOWER(name) LIKE ?', "%#{query.downcase}%").order(:name).first
+  end
 end
