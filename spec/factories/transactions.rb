094910357d0c6952f8f28ad1d1d2c3c82fe78b09
awosborn <alexander.w.osborne@gmail.com>
2021-02-16 19:47:01 -0700

    Add merchant revenue test

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -3,6 +3,6 @@ FactoryBot.define do
     invoice
     credit_card_number { Faker::Finance.credit_card }
     credit_card_expiration_date { "04/23" }
-    result { [:success, :failed, :refunded].sample }
+    results { ["success", "failed", "refunded"].sample }
   end
 end
